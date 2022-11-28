d690add8fdd5cf40c3c934a5f900cb69ed89a13e
YuriySN <you@example.com>
2022-11-28 06:58:25 +0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router';
+
+import Home from './pages/Home/Home'
+import MonthStatistics from './pages/MonthStatistics/MonthStatistics'
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/month-statistics' element={<MonthStatistics/>}/>
+      </Routes>
     </div>
   );
 }
